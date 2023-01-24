5369fe80368fadb5948e34821f6db0462eb8f656
LeslieAine <aineleslie@gmail.com>
2023-01-24 11:00:31 +0300

    Add books module for adding and removing books

---
+++ modules/book.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const formEl = document.getElementById('form');
+const titleEl = document.getElementById('title');
+const nameEl = document.getElementById('author');
+
+export default class BookCollection {
+  constructor() {
+    this.collection = JSON.parse(window.localStorage.getItem('bookCollection')) || [];
+  }
+
+  addBook(bookTitle, bookAuthor) {
+    const bookID = Math.ceil(Math.random() * 10000);
+    this.collection.push({ bookTitle, bookAuthor, bookID });
+    this.updateLocalStorage();
+  }
+
+  removeBook(bookID) {
+    this.collection = this.collection.filter((book) => book.bookID !== +bookID);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    window.localStorage.setItem('bookCollection', JSON.stringify(this.collection));
+  }
+}
